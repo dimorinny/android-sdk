5d4951664f8d4d06bd4616453a28a202d02f854e
dimorinny <didika914@gmail.com>
2016-11-25 00:58:32 +0300

    fix tests

--- test_environment.py
+++ test_environment.py
@@ -7,7 +7,7 @@ from hamcrest import assert_that, is_
 @pytest.mark.dockerfile()
 class TestEnvironment(object):
     def test_gcloud_exists(self, Command):
-        assert_that(Command('gcloud').rc, is_(0))
+        assert_that(Command('gcloud -v').rc, is_(0))
 
     def test_gsutil_exists(self, Command):
-        assert_that(Command('gsutil').rc, is_(0))
+        assert_that(Command('gsutil -v').rc, is_(0))
